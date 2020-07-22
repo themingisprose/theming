9de5082cb84aa382c4866fba2178d97de464a653
RogerTM <roger213tm@gmail.com>
2020-07-21 22:21:38 -0400

    The Loop

--- index.php
+++ index.php
@@ -10,8 +10,22 @@
 
 	<main class="flex-shrink-0">
 		<div class="container">
-			<h1 class="mt-5"><?php bloginfo( 'name' ) ?></h1>
+			<h1 class="mt-5"><a href="<?php echo home_url() ?>"><?php bloginfo( 'name' ) ?></a></h1>
 			<p class="lead"><?php bloginfo( 'description' ) ?></p>
+
+<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
+?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+				<?php the_content() ?>
+			</article>
+<?php
+				endwhile;
+			endif;
+
+?>
 		</div>
 	</main>
 
