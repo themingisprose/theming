5384c261c20e8ec2f56788ff03cb7f6f8258e2e6
rogertm <roger213tm@gmail.com>
2021-02-07 12:13:52 -0500

    Single Navigation

--- template-parts/content.php
+++ template-parts/content.php
@@ -25,3 +25,17 @@
 	<?php get_template_part( 'template-parts/entry-footer' ); ?>
 
 </article>
+
+<?php if ( is_singular() ) : ?>
+<div class="row my-5">
+
+	<div class="col-md-6 py-3">
+		<?php previous_post_link() ?>
+	</div>
+
+	<div class="col-md-6 py-3 text-end">
+		<?php next_post_link() ?>
+	</div>
+
+</div>
+<?php endif; ?>
