a2fa9d7111755bd22a234238fe0ec9b24c86ac60
rogertm <roger213tm@gmail.com>
2021-02-12 16:39:21 -0500

    Class Theming_Setting_Fields

--- classes/index.php
+++ classes/index.php
@@ -3,6 +3,7 @@
 
 // Admin classes
 require_once( get_template_directory() . '/classes/class.theming-admin.php' );
+require_once( get_template_directory() . '/classes/class.theming-admin-setting-fields.php' );
 
 // General Theme classes
 require_once( get_template_directory() . '/classes/class.theming-setup.php' );
