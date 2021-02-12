746c70e5c7a824ec0206e9a150a8dbaccabb3ca2
rogertm <roger213tm@gmail.com>
2021-02-11 21:21:04 -0500

    Sidebar & Widgets Area

--- classes/index.php
+++ classes/index.php
@@ -2,5 +2,7 @@
 // Silent is gold
 
 require_once( get_template_directory() . '/classes/class.theming-setup.php' );
+require_once( get_template_directory() . '/classes/class.theming-sidebar.php' );
 require_once( get_template_directory() . '/classes/class.theming-enqueue.php' );
+
 require_once( get_template_directory() . '/classes/class.theming-walker-comment.php' );
---
+++ sidebar.php
@@ -0,0 +1,30 @@
+<section id="sidebar" class="py-5 mb-5">
+	<div class="container">
+		<div class="row">
+		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+			<aside class="col-lg-3 col-md-6 col-12 text-center text-md-start footer-widgets">
+				<?php dynamic_sidebar( 'sidebar-1' ); ?>
+			</aside>
+		<?php endif; ?>
+
+		<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+			<aside class="col-lg-3 col-md-6 col-12 text-center text-md-start footer-widgets">
+				<?php dynamic_sidebar( 'sidebar-2' ); ?>
+			</aside>
+		<?php endif; ?>
+
+		<?php if ( is_active_sidebar( 'sidebar-3' ) ) : ?>
+			<aside class="col-lg-3 col-md-6 col-12 text-center text-md-start footer-widgets">
+				<?php dynamic_sidebar( 'sidebar-3' ); ?>
+			</aside>
+		<?php endif; ?>
+
+		<?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
+			<aside class="col-lg-3 col-md-6 col-12 text-center text-md-start footer-widgets">
+				<?php dynamic_sidebar( 'sidebar-4' ); ?>
+			</aside>
+		<?php endif; ?>
+
+		</div>
+	</div>
+</section>
