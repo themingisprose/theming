f1b16a3c7abe0664d66a8c2b075526be53950648
rogertm <roger213tm@gmail.com>
2021-02-11 20:36:14 -0500

    HTML Footer & Colophon

--- footer.php
+++ footer.php
@@ -8,6 +8,31 @@
 
 		</main>
 
+		<footer id="site-footer" class="footer">
+
+			<?php get_sidebar() ?>
+
+			<div id="colophon" class="py-4">
+
+				<div class="container">
+
+					<div id="copyright" class="footer-copyright">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+						&copy;
+						<?php
+						echo date_i18n(
+							/* translators: Copyright date format, see https://www.php.net/date */
+							_x( 'Y', 'copyright date format', 'rex' )
+						);
+						?>
+					</div>
+
+				</div>
+
+			</div>
+
+		</footer>
+
 		<?php wp_footer(); ?>
 
 	</body>
