03febdaa2e00d3adb5d61e1a0678e69aba767f3a
rogertm <roger213tm@gmail.com>
2021-04-02 11:42:41 -0400

    Class Theming_SVG_Icons

--- classes/index.php
+++ classes/index.php
@@ -8,6 +8,7 @@ require_once( get_template_directory() . '/classes/class.theming-admin-social-op
 
 // General Theme classes
 require_once( get_template_directory() . '/classes/class.theming-enqueue.php' );
+require_once( get_template_directory() . '/classes/class.theming-svg-icons.php' );
 require_once( get_template_directory() . '/classes/class.theming-walker-comment.php' );
 
 // External classes
