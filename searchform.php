69848ac1ac75b215fc982684af1c6b4369d5bde4
rogertm <roger213tm@gmail.com>
2021-05-02 12:19:43 -0400

    Bug Fixed: esc_url( home_url( '/' ) )

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
  * @since Theming_ 0.0.1
  */
 ?>
-<form id="searchform" class="row g-0" action="<?php echo home_url( '/' ); ?>" method="get">
+<form id="searchform" class="row g-0" action="<?php echo esc_url( home_url( '/' ) ) ?>" method="get">
 	<div class="col-9">
 		<div class="input-group">
 			<input type="text" class="form-control" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="<?php _e( 'Search', 'theming' ) ?>" />
