7ff39e2d4f75ab3d6fb070a3b87654cea185b835
rogertm <roger213tm@gmail.com>
2021-05-02 18:03:39 -0400

    Class Theming_Enqueue sin verificacion

--- classes/class.theming-enqueue.php
+++ classes/class.theming-enqueue.php
@@ -4,68 +4,67 @@
  *
  * @since Theming_ 0.0.1
  */
-if ( ! class_exists( 'Theming_Enqueue' ) ) {
-	class Theming_Enqueue {
+class Theming_Enqueue
+{
 
-		/**
-		 * @var null
-		 */
-		protected static $instance = null;
+	/**
+	 * @var null
+	 */
+	protected static $instance = null;
 
-		/**
-		 * Enqueue
-		 *
-		 * @since Theming_ 0.0.1
-		 */
-		public function init()
-		{
-			add_action( 'wp_enqueue_scripts', array( $this, 'styles' ) );
-			add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
-		}
-
-		/**
-		 * Handle styles
-		 *
-		 * @since Theming_ 0.0.1
-		 */
-		public function styles()
-		{
-			wp_register_style( 'theming-style', get_theme_file_uri( '/assets/dist/css/style.css' ) );
-			wp_enqueue_style( 'theming-style' );
-		}
-
-		/**
-		 * Handle scripts
-		 *
-		 * @since Theming_ 0.0.1
-		 */
-		public function scripts()
-		{
-			wp_register_script( 'theming-bootstrap', get_theme_file_uri( 'assets/dist/js/bootstrap.js' ) );
-			wp_enqueue_script( 'theming-bootstrap' );
-
-			/**
-			 * Adds JavaScript to pages with the comment form to support
-			 * sites with threaded comments (when in use).
-			 */
-			if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) :
-				wp_enqueue_script( 'comment-reply' );
-			endif;
-		}
-
-		/**
-		 * Get Instance
-		 *
-		 * @since Theming_ 0.0.1
-		 */
-	    public static function get_instance() : self
-	    {
-	        if (null === self::$instance) {
-	            self::$instance = new self();
-	        }
-	        return self::$instance;
-	    }
+	/**
+	 * Enqueue
+	 *
+	 * @since Theming_ 0.0.1
+	 */
+	public function init()
+	{
+		add_action( 'wp_enqueue_scripts', array( $this, 'styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
 	}
 
-	Theming_Enqueue::get_instance()->init();
+	/**
+	 * Handle styles
+	 *
+	 * @since Theming_ 0.0.1
+	 */
+	public function styles()
+	{
+		wp_register_style( 'theming-style', get_theme_file_uri( '/assets/dist/css/style.css' ) );
+		wp_enqueue_style( 'theming-style' );
+	}
+
+	/**
+	 * Handle scripts
+	 *
+	 * @since Theming_ 0.0.1
+	 */
+	public function scripts()
+	{
+		wp_register_script( 'theming-bootstrap', get_theme_file_uri( 'assets/dist/js/bootstrap.js' ) );
+		wp_enqueue_script( 'theming-bootstrap' );
+
+		/**
+		 * Adds JavaScript to pages with the comment form to support
+		 * sites with threaded comments (when in use).
+		 */
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) :
+			wp_enqueue_script( 'comment-reply' );
+		endif;
+	}
+
+	/**
+	 * Get Instance
+	 *
+	 * @since Theming_ 0.0.1
+	 */
+    public static function get_instance() : self
+    {
+        if (null === self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
 }
+
+Theming_Enqueue::get_instance()->init();
