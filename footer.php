7a57585e7b0d53ce256c1cac7d16d99a85315b42
rogertm <roger213tm@gmail.com>
2022-05-20 15:35:19 -0400

    Bug Fixed: Arreglados errores basandos en la validacion de W3c

--- footer.php
+++ footer.php
@@ -37,7 +37,6 @@
 							'container_id'		=> 'the-footer-menu',
 							'menu_class'		=> 'list-inline',
 							'item_class'		=> 'list-inline-item',
-							'items_wrap'		=> '%3$s',
 							'depth'				=> 1,
 							'link_before'		=> '<span>',
 							'link_after'		=> '</span>',
--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 <html class="no-js" <?php language_attributes() ?>>
 
 	<head>
-		<meta charset="<?php bloginfo( 'chartset' ) ?>">
+		<meta charset="<?php bloginfo( 'charset' ) ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
@@ -17,11 +17,11 @@
 		<?php wp_head(); ?>
 	</head>
 
-	<body <?php body_class() ?> data="no-jquery">
+	<body <?php body_class() ?>>
 
 		<?php wp_body_open(); ?>
 
-			<header id="site-header" role="banner">
+			<header id="site-header">
 				<?php do_action( 'theming_site_header' ) ?>
 			</header>
 
